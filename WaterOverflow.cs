c295ad67ab7fd75803c3675f73cfb8c1170033c9
ivan Gyoshev <ivan_gy6ev@abv.bg>
2017-06-05 23:03:33 +0300

    WaterOverflow

---
+++ WaterOverflow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.WaterOverflow
+{
+    class WaterOverflow
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int liters = 0;
+            int volume = 255;
+            for (int i=1; i<=n; i++)
+            {
+                liters = int.Parse(Console.ReadLine());
+                if (volume - liters < 0) Console.WriteLine("Insufficient capacity!");
+                else volume -= liters;                
+            }
+            Console.WriteLine(255 - volume);
+        }
+    }
+}
