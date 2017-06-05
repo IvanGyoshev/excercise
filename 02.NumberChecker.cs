0e20a0261cf452a23169c0d44e720ead134cc937
ivan Gyoshev <ivan_gy6ev@abv.bg>
2017-06-05 23:02:49 +0300

    NumberChecker

---
+++ 02.NumberChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.NumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number= double.Parse(Console.ReadLine());
+            bool result = (number % 1 == 0); //check for floating point number
+            if (result) Console.WriteLine("integer");
+            else Console.WriteLine("floating-point");
+        }
+    }
+}
